2a366cd5627621406852319ae75f222e89061bb2
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-07-31 09:16:15 -0600

    update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const clientId = 'hkijppeag6gwuykcpbkhuyj2dtga1ckb';
 const clientSecret = '2fCkF7Yq6Gg8QB5QSiqBV9yLPDX8H6l8'; // You should never expose the client secret on the client-side. Use a server-side application to keep it secure.
 const redirectUri = 'https://dev.morganserver.com';
-const authorizationCode = 'DX1585fdRnCGaoZkKUOR5bXAtnJuoFBm';
+const authorization_Code = 'DX1585fdRnCGaoZkKUOR5bXAtnJuoFBm';
 
 // Token endpoint URL
 const tokenUrl = 'https://api.box.com/oauth2/token';
@@ -9,7 +9,7 @@ const tokenUrl = 'https://api.box.com/oauth2/token';
 // Prepare the data for the token request
 const formData = new FormData();
 formData.append('grant_type', 'authorization_code');
-formData.append('code', authorizationCode);
+formData.append('code', authorization_Code);
 formData.append('client_id', clientId);
 formData.append('client_secret', clientSecret);
 formData.append('redirect_uri', redirectUri);
